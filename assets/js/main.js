0ff03bb8271851ef2ee18bb8042fec1a9cf34728
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-03 13:59:26 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+window.alert("Hallo Welt");
+let name = window.prompt("Bitte geben Sie Ihren Namen ein.");
+console.log(name);
+document.write(name);
+let nachName =window.prompt("Bitte geben Sie Ihren Nachnamen ein");
+console.log(nachName);
+document.write(nachName);
+let confirmation = window.confirm("Stimmen Sie meiner Meinung zu?"); 
+console.log(confirmation);
+document.write(confirmation);
+
+let alter = prompt( "Gib mir bitte dein Alter:" );
+console.log(alter);
+document.write(alter);
+
+
+let b = 5;
+let a = b * 5 -3;
+alert(a);
+
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
